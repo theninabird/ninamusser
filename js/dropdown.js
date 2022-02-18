a12aab0d599ab2534f2159c5ddd61189e5479c3c
Nina <ninamusser@gmail.com>
2022-02-18 14:16:39 -0500

    update

--- js/dropdown.js
+++ js/dropdown.js
@@ -10,4 +10,22 @@ $(document).ready(function() {
         	$('.dropdown').slideToggle(100);
     	}
 	);
+
+	$('#backtotop').on('click', function(){
+		$("html, body").animate({ scrollTop: 0 }, "slow");
+		$('#backtotop').fadeOut();
+		return false;
+	});
+});
+
+$(window).on('scroll', function(){
+	if ( $('#backtotop').is(":hidden") ) {
+		setTimeout(function(){
+			$('#backtotop').css('display', 'flex');
+		}, 1000);
+	}
+
+	if ( scrollTop == 0 ) {
+		$('#backtotop').fadeOut();
+	}
 });
